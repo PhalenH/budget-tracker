f6f1fe920022b133b630b6d9e9cc061d7fc337f6
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-01-21 20:47:55 -0500

    added install portion of cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,29 @@
-console.log("checking status")
+console.log("checking status");
+
+// resources to build application
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+// Prevent caches from using outdated cache (react manages this automatically)
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// what is the event install, is that referencing service-worker?
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Successfully pre-cached file");
+      // installs all the initial resources and put it in cache
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  // tell the browser to activate this service worker immediately once it has finished installing
+  self.skipWaiting();
+});
