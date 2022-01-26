e9497b3c5d2983cdbb97d7d95f078b3ed686bbc5
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-01-25 20:09:44 -0500

    formatted index.js and api.js, added a db.js to be filled in with indexdb code, added fetch cache code in serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -28,20 +28,61 @@ self.addEventListener("install", function (event) {
   self.skipWaiting();
 });
 
-//activate the service worker and remove old data from the cache.
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
+// clears the cache and reactivates the application once it's done
+// activate the service worker and remove old data from the cache.
+// runs before the install, makes sure all the version numbers stay the same
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Old cache removed", key);
+            // removes old cache
             return caches.delete(key);
           }
         })
       );
     })
   );
-
+  // will not call the server if file is in that cache
   self.clients.claim();
+});
+
+// handles requests
+self.addEventListener("fetch", function (event) {
+  // checks if api request, and if so will cache a successful request
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // checks if response was good, if it is, copies and caches request
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              // if error, tries to get it from cache
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return
+  }
+
+  // if we are offline or if we need something from the api but are still offline
+  // this is the catch event
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      // so if the request isn't in the response then it's fetched? does it get cached then?
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
